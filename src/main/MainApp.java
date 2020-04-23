55ec8201e061cf84fa3aad0d2b7b079dc3bd5d65
Tindang <trungtincse@gmail.com>
2020-04-23 17:01:27 +0700

    a

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -5,18 +5,12 @@
  */
 package main;
 
-import com.google.common.util.concurrent.RateLimiter;
-import com.vng.zing.logger.ZLogger;
-import com.vng.zing.stats.Profiler;
-import org.apache.log4j.Logger;
 
 /**
  *
  * @author tindang
  */
 public class MainApp {
-    private static final Logger _Logger = ZLogger.getLogger(MainApp.class); 
-    public static RateLimiter ralimiter = RateLimiter.create(5000);
     public static void main(String[] args) {
         Exception ex=new Exception();
         System.out.println("Hello");
